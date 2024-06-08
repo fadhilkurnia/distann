8280827d6285b827abdea4c43484a517081184e6
Rihui Lu <rihuilu1108@gmail.com>
2024-06-08 13:58:55 -0400

    temporary images for output

--- Frontend Implementation/script.js
+++ Frontend Implementation/script.js
@@ -1,18 +1,13 @@
 var promt; // stores the prompt to conver to text description
 var time; // stores the time
 
-const http = new XMLHttpRequest();
-const url = 'https://cocodataset.org/#explore'; // the url of the server
-http.open("GET", url);
-http.send();
 
+fetch('https://cocodataset.org/#explore')
+.then(res => (res.ok ? res.text() : Promise.reject(new Error(res.statusText))))
+.then(data => {
+    console.log(data);
+  });
 
-
-//testing
-http.onreadystatechange = (e) => {
-    console.log(http.responseText)
-  }
-  
 function send(){
     //stores the promt in the global variable to be converted later on 
     promt = document.getElementById("input").value;
