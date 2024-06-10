5667ab242f2d131df4a20fe505cbaa1b96cc2499
sami0ahmed <sami872344@gmail.com>
2024-06-10 19:05:59 -0400

    Setup initial drogon framework and got the server to respond with  image

---
+++ http_server/build/CMakeFiles/http_server.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/samiahmed/Projects/URV/distann/http_server/controllers/TestCtrl.cc" "CMakeFiles/http_server.dir/controllers/TestCtrl.cc.o" "gcc" "CMakeFiles/http_server.dir/controllers/TestCtrl.cc.o.d"
+  "/Users/samiahmed/Projects/URV/distann/http_server/main.cc" "CMakeFiles/http_server.dir/main.cc.o" "gcc" "CMakeFiles/http_server.dir/main.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ http_server/build/CMakeFiles/http_server.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/http_server.dir/controllers/TestCtrl.cc.o"
+  "CMakeFiles/http_server.dir/controllers/TestCtrl.cc.o.d"
+  "CMakeFiles/http_server.dir/main.cc.o"
+  "CMakeFiles/http_server.dir/main.cc.o.d"
+  "http_server"
+  "http_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/http_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
