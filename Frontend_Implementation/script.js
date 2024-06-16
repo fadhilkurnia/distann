f7492efb2cd13e799e582d379fbcba56ec5d6860
Rihui Lu <rihuilu1108@gmail.com>
2024-06-15 21:35:13 -0400

    comments for js

--- Frontend_Implementation/script.js
+++ Frontend_Implementation/script.js
@@ -1,16 +1,11 @@
-var promt; // stores the prompt to conver to text description
-var time; // stores the time
-
-
-fetch('https://cocodataset.org/#explore')
-.then(res => (res.ok ? res.text() : Promise.reject(new Error(res.statusText))))
-.then(data => {
-    console.log(data);
-  });
+var promt; // stores the prompt to convert to text description
 
 function send(){
     //stores the promt in the global variable to be converted later on 
+    //sends the a request to the backend
+    //waits for the response from the backend
     promt = document.getElementById("input").value;
-
-
+    
+    //received the response from the backend and set the img1 - img10 to the images that we get from the backend 
+    //if there is text: set the text to the description that we get from the backend
 }
