d926ec41ae1a39cee3d1577400493ba01c43648f
Rihui Lu <rihuilu1108@gmail.com>
2024-06-21 13:29:24 -0700

    Updated Js

--- Frontend_Implementation/script.js
+++ Frontend_Implementation/script.js
@@ -5,7 +5,13 @@ function send(){
     //sends the a request to the backend
     //waits for the response from the backend
     promt = document.getElementById("input").value;
-    
+    console.log("prmpt: " + promt);
+
+    fetch("/test", )
+    .then(res => (res.ok ? res.json() : Promise.reject(new Error(res.statusText))))
+    .then((data) => {
+        console.log(data);
+    });
     //received the response from the backend and set the img1 - img10 to the images that we get from the backend 
     //if there is text: set the text to the description that we get from the backend
 }
