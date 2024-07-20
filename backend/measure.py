c6017c25767213d845ac34c151327a6addf60adf
Rihui <ubuntu@Squid>
2024-07-20 10:53:16 -0700

    cddf

--- backend/measure.py
+++ backend/measure.py
@@ -2,22 +2,56 @@ import subprocess
 import requests
 import time
 import argparse
+import numpy as np
+import matplotlib.pyplot as plt 
 
-def send_requests(ports, num_requests = 1, delay = 1):
+
+def calculate_cdf(latencies): 
+    sotr = np.sort(latencies)
+    cdf = np.linspace(0, 1, len(latencies))
+
+    #plot 
+    plt.xlabel('time') 
+    plt.ylabel('cdf') 
+    plt.title('CDF of response time')
+    plt.plot(sotr, cdf, marker = 'o')
+    plt.savefig('plot.png')
+    print("image saved!")
+    subprocess.Popen('imview plot.png', shell = True)
+    return 
+    
+# def calculate_latency_increasing_thoughput(time, bytes):
+#     throughput = bytes/time
+#     print(f"Total Time Taken: {time:.2f} seconds")
+#     print(f"Throughput: {throughput / (1024 * 1024):.2f} MBps")
+#     return throughput
+
+def send_requests(ports, serving_appro, num_requests = 1, throughput_levels = [1], duration = 10):
     processes = []
+    latencies = {port: [] for port in ports}
+
     for port in ports:
-        print(f"Starting server on port {port}")
+
+        print(f"Starting server on port: {port}, using serving approach: {serving_appro}")
         process = subprocess.Popen(['build/backend', str(port)]) 
-        time.sleep(delay)
+        #process = subprocess.Popen(['build/backend', str(port), str(serving_appro)])
+        time.sleep(1)
         for _ in range(num_requests):
             try:
+                start_time = time.time()
                 response = requests.get(f'http://localhost:{port}')
+                end_time = time.time()
+                latency = end_time - start_time
+                latencies[port].append(latency)
+
                 print(f"Port:{port}, Status Code: {response.status_code}, Response Time: {response.elapsed.total_seconds()} seconds")
             except requests.RequestException as e:
                 print(f"Request failed for port {port}: {e}")
-            time.sleep(1)
 
-        processes.append(process)    
+        calculate_cdf(latencies[port])
+        processes.append(process) 
+
+    print("All servers started")   
     return processes
 
 def close_processes(processes):
@@ -29,13 +63,18 @@ def close_processes(processes):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Send multiple requests to a backend")
     parser.add_argument("--ports", nargs = '+', type = int, required = True, help = "List of ports for the servers")
-    parser.add_argument("--num_requests", type=int, default=1, help="Number of requests to send")    
-    parser.add_argument("--delay", type=float, default=1, help="Delay between requests")
+    parser.add_argument("--num_requests", type=int, default=1, help="Number of requests to send")   
+
+    # parser.add_argument("--throughput_levels", nargs='+', type=float, required=True, help="List of throughput levels (requests per second)")
+    # parser.add_argument("--duration", type=int, default=10, help="Duration to test each throughput level (seconds)")
+ 
+    #set serving_appro to to required after implementing the serving approach
+    parser.add_argument("--serving_appro", type=int, default=0, help="Serving approach")
 
     args = parser.parse_args()
     
     try:
-        processes = send_requests(args.ports, args.num_requests, args.delay)
+        processes = send_requests(args.ports, args.serving_appro, args.num_requests)
         print("Press Ctrl+C to stop the servers")
         while True:
             time.sleep(1)
